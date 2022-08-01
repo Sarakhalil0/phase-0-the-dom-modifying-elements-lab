9038e7f16e80ad5e3c60edd6675bcefa90dfd253
sara khalil <sarakhalil3377@gmail.com>
2022-08-02 00:04:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+
+//first one
+const element = document.getElementById('main');
+element.remove();
+
+//second one
+const newHeader = document.createElement("h1");
+const divToMoveTo = document.getElementById("victory");
+
+//third one
+
+newHeader.id = 'victory'
+//forth one
+newHeader.innerHTML = "YOUR-NAME is the champion"
+
